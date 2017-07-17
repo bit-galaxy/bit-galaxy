770626f21d69c1671f67a7b51e2265b37b3e2dc0
joe-denea <jdenea@outlook.com>
2017-07-17 12:46:26 -0700

    Fix webpack pathing

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ const config = {
   output: {
     path: __dirname,
     filename: 'build/bundle.js',
-    publicPath: '/',
+    publicPath: '/bit-galaxy',
   },
   plugins: (process.env.NODE_ENV === 'production') ? [
     ...standardPlugins,
